cc2d099c8fbe11ddf59133c11ea9939bd6c28042
Voidmaster <voidmaster666@gmail.com>
2016-03-15 13:43:45 +0300

    cat now can read from file

--- cat/cat.c
+++ cat/cat.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 
 const int BUFFER_SIZE = 2048;
 
@@ -7,7 +9,13 @@ int _read(int fd, void *buf, size_t buf_size) {
     int count = 0;
     while (buf_size) {
         int res = read(fd, buf, buf_size);
-        if (res < 0) return -1;
+        if (res == -1) {
+            if (errno == EINTR) {
+                continue;
+            } else {
+                return -1;
+            }
+        }
         if (res == 0) break;
 
         buf_size -= res;
@@ -22,7 +30,13 @@ int _write(int fd, void *buf, size_t buf_size) {
     int count = 0;
     while (buf_size) {
         int res = write(fd, buf, buf_size);
-        if (res < 0) return -1;
+        if (res == -1) {
+            if (errno == EINTR) {
+                continue;
+            } else {
+                return -1;
+            }
+        }
         if (res == 0) break;
 
         buf_size -= res;
@@ -33,14 +47,31 @@ int _write(int fd, void *buf, size_t buf_size) {
     return count;
 }
 
-int main() {
+int cat(int fd) {
     char buf[BUFFER_SIZE];
     while (1) {
-        int res = _read(STDIN_FILENO, buf, BUFFER_SIZE);
+        int res = _read(fd, buf, BUFFER_SIZE);
         if (res < 0) return -1;
         if (res == 0) break;
         res = _write(STDOUT_FILENO, buf, res);
         if (res < 0) return -1;
     }
+
     return 0;
 }
+
+int main(int argc, char *argv[]) {
+    if (argc >= 2) {
+        int i;
+        for (i = 1; i < argc; i++) {
+            if (cat(open(argv[i], O_RDONLY)) == -1) {
+                return -1;
+            }
+        }
+    } else {
+        return cat(STDIN_FILENO);
+    }
+    
+    return 0;
+}
+
